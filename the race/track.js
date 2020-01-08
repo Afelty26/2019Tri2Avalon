49e088d54a9eba34f4b4b45120239dc9f0c69c53
Afelty26 <afelty26@nscds.org>
2020-01-08 17:16:40 +0000

    car

--- the race/track.js
+++ the race/track.js
@@ -1,7 +1,7 @@
 init();
 
 function init(){
-    for(var r=0; r<12; r++)
+    for(var r=0; r<10; r++)
         for(var c=0; c<4; c++){
             var color = (r+c)%2===0?' black': ' white';
             $('#finishLine').append("<div class='checker " + color + "'></div>");
@@ -12,6 +12,9 @@ function init(){
 function startRace(){
     TweenMax.to("#car1",2,{x:520,ease:Sine.easeOut});
     TweenMax.to("#car1",1.5,{x:500, y:-320,delay:1.7, ease:Sine.easeOut});
-    TweenMax.to("#car1",2,{x:-50, y:-300,delay:3, ease:Sine.easeOut});
+    TweenMax.to("#car1",2,{x:-90, y:-300,delay:3, ease:Sine.easeOut});
+     TweenMax.to("#car1",2,{x:-90, y:-0,delay:5, ease:Sine.easeOut});
+    TweenMax.to("#car1",2,{x:90,y: 90,delay:7,ease:Sine.easeOut});
+
 }
 
