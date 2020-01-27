0f7ccdd170362f743ece597248bea0b4f06bf348
Afelty26 <afelty26@nscds.org>
2020-01-27 19:09:55 +0000

    ewdf

--- the race/track.js
+++ the race/track.js
@@ -24,11 +24,11 @@ function startRace(){
     
     TweenMax.to("#car2",2,{x:490,ease:Sine.easeOut});
      TweenMax.to("#car2",.8,{rotation:-90,delay:1,ease:Sine.easeOut});
-     TweenMax.to("#car2",1.5,{x:500, y:-320,delay:1.6, ease:Sine.easeOut});
-      TweenMax.to("#car2",.5,{rotation:-180,delay:2,ease:Sine.easeOut});
-    TweenMax.to("#car2",1.6,{x:-100, y:-280,delay:2.6, ease:Sine.easeOut});
-    /*TweenMax.to("#car2",.5,{rotation:-270,delay:4.2,ease:Sine.easeOut});
-     TweenMax.to("#car2",2,{x:-85, y:-0,delay:4.7, ease:Sine.easeOut});
+     TweenMax.to("#car2",.5,{x:500, y:-320,delay:1.6, ease:Sine.easeOut});
+      TweenMax.to("#car2",1,{rotation:-180,delay:1.8,ease:Sine.easeOut});
+    TweenMax.to("#car2",1,{x:-100, y:-280,delay:2.2, ease:Sine.easeOut});
+    TweenMax.to("#car2",.5,{rotation:-270,delay:3,ease:Sine.easeOut});
+     /*TweenMax.to("#car2",2,{x:-85, y:-0,delay:4.7, ease:Sine.easeOut});
      TweenMax.to("#car2",.5,{rotation:-360,delay:5.2,ease:Sine.easeOut});
     TweenMax.to("#car2",2,{x:90,y: 0,delay:6.5,ease:Sine.easeOut});*/
 
